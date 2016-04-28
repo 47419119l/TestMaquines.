70f55129678d6511e1301caaef4e1ff78e605897
47419119l <47419119l@iespoblenou.org>
2016-04-28 17:09:42 +0200

    Lista pasos a seguir de maquinas funciona correctamente.

--- app/src/main/java/com/example/sandra/testmaquines/MainActivity.java
+++ app/src/main/java/com/example/sandra/testmaquines/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.sandra.testmaquines;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.util.Linkify;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.ArrayList;
 
@@ -20,9 +29,11 @@ public class MainActivity extends AppCompatActivity {
     private Firebase maquinaRef;
     private Firebase ref;
     private ImageButton buttonReader;
+    private String idmaquina;
     private ListView listStep;
     private TextView nom;
     private TextView desc;
+    private ArrayAdapterStep adapter;
 
 
     @Override
@@ -52,11 +63,86 @@ public class MainActivity extends AppCompatActivity {
         buttonReader = (ImageButton) findViewById(R.id.qrlectormaquina);
         listStep = (ListView)findViewById(R.id.listStep);
         nom = (TextView)findViewById(R.id.nomMaquina);
-        desc = (TextView)findViewById(R.id.descripciomaquina);
+        configuracioButoQr();
 
 
 
 
+    }
+    private void configuracioButoQr() {
+        buttonReader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    //new IntentIntegrator(QrReaderActity.this).initiateScan();
+                    Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                    intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                    startActivityForResult(intent, 0);
+                } catch (ActivityNotFoundException exception) {
+
+
+                }
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if((requestCode == 0) && (resultCode == -1)) {
+            updateUITextViews(data.getStringExtra("SCAN_RESULT"), data.getStringExtra("SCAN_RESULT_FORMAT"));
+        } else {
+
+        }
+    }
+
+    private void handleResult(IntentResult scanResult) {
+        if (scanResult != null) {
+            updateUITextViews(scanResult.getContents(), scanResult.getFormatName());
+        } else {
+            Toast.makeText(getBaseContext(), "No s'ha pogut lleguir cap codi Qr", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void updateUITextViews(String scan_result, String scan_result_format) {
+
+        idmaquina = scan_result;
+        Query queryRef = maquinaRef.orderByChild("id").equalTo(idmaquina);
+        queryRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot snapshot, String previousChild) {
+               Maquina a=  snapshot.getValue(Maquina.class);
+                nom.setText(a.getNom());
+                ArrayList steps = a.getSteps();
+
+                 adapter = new ArrayAdapterStep(
+                 getBaseContext(),
+                 R.layout.list_maquines_util,
+                 steps
+                 );
+                listStep.setAdapter(adapter);
+
+
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 
     @Override
